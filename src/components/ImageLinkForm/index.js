be6f945e8ad34eecd0fb678171aae5480dc6bf67
sofiane-benmahiddine <sof.benmahiddine@gmail.com>
2022-02-05 11:40:49 +0100

    change input style

--- src/components/ImageLinkForm/index.js
+++ src/components/ImageLinkForm/index.js
@@ -9,12 +9,12 @@ export default function ImageLinkForm({ onInputChange, onButtonSubmit }) {
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
           <input
-            className="f4 pa2 w-70 center"
+            className="f4 pa2 w-70 center bw2"
             type="text"
             onChange={onInputChange}
           />
           <button
-            className="w-30 grow f4 bn link ph3 pv2 dib white bg-light-purple"
+            className="w-25 grow f4 fw6 bn link ph3 pv2 dib near-black bg-washed-yellow"
             onClick={onButtonSubmit}
           >
             Detect
