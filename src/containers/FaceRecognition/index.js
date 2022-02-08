ccce75ccad4d4f6e1876fc334858793263a1209a
sofiane-benmahiddine <sof.benmahiddine@gmail.com>
2022-02-08 17:48:01 +0100

    update server url

--- src/containers/FaceRecognition/index.js
+++ src/containers/FaceRecognition/index.js
@@ -46,7 +46,6 @@ export default function FaceRecognition({ imgUrl, isLoading, setIsLoading }) {
               ></div>
             );
           })}
-        {isLoading && <p>Finding faces ...</p>}
       </div>
     </div>
   );
